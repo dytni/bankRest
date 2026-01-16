81ff2ed205887c987af4af5e1b136f83c1b239d0
nikolaj <nikolajolesko391@gmail.com>
2026-01-16 06:16:03 +0300

    -Add main OpenApiConfig

---
+++ src/main/java/com/example/bankcards/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.example.bankcards.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Bank API",
+                version = "1.0.0",
+                description = "API for managing bank cards"
+        ),
+        security = @SecurityRequirement(name = "JWT")
+)
+@SecurityScheme(
+        name = "JWT",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        description = "Insert your Access Token here"
+)
+public class OpenApiConfig {
+
+}
